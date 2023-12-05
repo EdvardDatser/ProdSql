0b5ebca3b300dde37796748b38ba13a28e1352f2
opilane <opilane@HP-CZC2349HT2>
2023-12-05 12:15:32 +0200

    Pochti vse working

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -40,23 +40,28 @@
             this.Lisabtn = new System.Windows.Forms.Button();
             this.addAllbtn = new System.Windows.Forms.Button();
             this.refresh = new System.Windows.Forms.Button();
-            this.Kustuta = new System.Windows.Forms.Button();
+            this.kustutabtn = new System.Windows.Forms.Button();
             this.KustutaKat = new System.Windows.Forms.Button();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.imagebtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // dgv
             // 
             this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv.Location = new System.Drawing.Point(71, 221);
+            this.dgv.Location = new System.Drawing.Point(42, 285);
             this.dgv.Name = "dgv";
-            this.dgv.Size = new System.Drawing.Size(639, 164);
+            this.dgv.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgv.Size = new System.Drawing.Size(1077, 307);
             this.dgv.TabIndex = 0;
+            this.dgv.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgv_RowHeaderMouseClick);
             // 
             // KategooriaBox
             // 
             this.KategooriaBox.FormattingEnabled = true;
-            this.KategooriaBox.Location = new System.Drawing.Point(178, 151);
+            this.KategooriaBox.Location = new System.Drawing.Point(181, 180);
             this.KategooriaBox.Name = "KategooriaBox";
             this.KategooriaBox.Size = new System.Drawing.Size(160, 21);
             this.KategooriaBox.TabIndex = 1;
@@ -66,7 +71,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(67, 151);
+            this.label1.Location = new System.Drawing.Point(70, 180);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 21);
             this.label1.TabIndex = 2;
@@ -77,7 +82,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(117, 108);
+            this.label2.Location = new System.Drawing.Point(120, 123);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(50, 21);
             this.label2.TabIndex = 4;
@@ -88,7 +93,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(104, 67);
+            this.label3.Location = new System.Drawing.Point(107, 80);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 21);
             this.label3.TabIndex = 5;
@@ -98,7 +103,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(104, 23);
+            this.label4.Location = new System.Drawing.Point(107, 29);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(63, 21);
             this.label4.TabIndex = 6;
@@ -107,7 +112,7 @@
             // HindBox
             // 
             this.HindBox.FormattingEnabled = true;
-            this.HindBox.Location = new System.Drawing.Point(178, 108);
+            this.HindBox.Location = new System.Drawing.Point(181, 125);
             this.HindBox.Name = "HindBox";
             this.HindBox.Size = new System.Drawing.Size(160, 21);
             this.HindBox.TabIndex = 7;
@@ -115,7 +120,7 @@
             // KogusBox
             // 
             this.KogusBox.FormattingEnabled = true;
-            this.KogusBox.Location = new System.Drawing.Point(178, 67);
+            this.KogusBox.Location = new System.Drawing.Point(181, 80);
             this.KogusBox.Name = "KogusBox";
             this.KogusBox.Size = new System.Drawing.Size(160, 21);
             this.KogusBox.TabIndex = 8;
@@ -123,14 +128,14 @@
             // ToodeBox
             // 
             this.ToodeBox.FormattingEnabled = true;
-            this.ToodeBox.Location = new System.Drawing.Point(178, 25);
+            this.ToodeBox.Location = new System.Drawing.Point(181, 31);
             this.ToodeBox.Name = "ToodeBox";
             this.ToodeBox.Size = new System.Drawing.Size(160, 21);
             this.ToodeBox.TabIndex = 9;
             // 
             // Lisabtn
             // 
-            this.Lisabtn.Location = new System.Drawing.Point(344, 151);
+            this.Lisabtn.Location = new System.Drawing.Point(347, 178);
             this.Lisabtn.Name = "Lisabtn";
             this.Lisabtn.Size = new System.Drawing.Size(101, 23);
             this.Lisabtn.TabIndex = 10;
@@ -140,7 +145,7 @@
             // 
             // addAllbtn
             // 
-            this.addAllbtn.Location = new System.Drawing.Point(71, 194);
+            this.addAllbtn.Location = new System.Drawing.Point(74, 240);
             this.addAllbtn.Name = "addAllbtn";
             this.addAllbtn.Size = new System.Drawing.Size(101, 23);
             this.addAllbtn.TabIndex = 12;
@@ -150,26 +155,26 @@
             // 
             // refresh
             // 
-            this.refresh.Location = new System.Drawing.Point(178, 194);
+            this.refresh.Location = new System.Drawing.Point(181, 240);
             this.refresh.Name = "refresh";
             this.refresh.Size = new System.Drawing.Size(101, 23);
             this.refresh.TabIndex = 13;
             this.refresh.Text = "Uuenda";
             this.refresh.UseVisualStyleBackColor = true;
             // 
-            // Kustuta
+            // kustutabtn
             // 
-            this.Kustuta.Location = new System.Drawing.Point(285, 194);
-            this.Kustuta.Name = "Kustuta";
-            this.Kustuta.Size = new System.Drawing.Size(101, 23);
-            this.Kustuta.TabIndex = 14;
-            this.Kustuta.Text = "Kustuta";
-            this.Kustuta.UseVisualStyleBackColor = true;
-            this.Kustuta.Click += new System.EventHandler(this.button3_Click);
+            this.kustutabtn.Location = new System.Drawing.Point(288, 240);
+            this.kustutabtn.Name = "kustutabtn";
+            this.kustutabtn.Size = new System.Drawing.Size(101, 23);
+            this.kustutabtn.TabIndex = 14;
+            this.kustutabtn.Text = "Kustuta";
+            this.kustutabtn.UseVisualStyleBackColor = true;
+            this.kustutabtn.Click += new System.EventHandler(this.button3_Click);
             // 
             // KustutaKat
             // 
-            this.KustutaKat.Location = new System.Drawing.Point(451, 151);
+            this.KustutaKat.Location = new System.Drawing.Point(454, 178);
             this.KustutaKat.Name = "KustutaKat";
             this.KustutaKat.Size = new System.Drawing.Size(105, 23);
             this.KustutaKat.TabIndex = 15;
@@ -177,13 +182,33 @@
             this.KustutaKat.UseVisualStyleBackColor = true;
             this.KustutaKat.Click += new System.EventHandler(this.KustutaKat_Click);
             // 
+            // PictureBox
+            // 
+            this.PictureBox.Location = new System.Drawing.Point(774, 12);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(297, 251);
+            this.PictureBox.TabIndex = 16;
+            this.PictureBox.TabStop = false;
+            // 
+            // imagebtn
+            // 
+            this.imagebtn.Location = new System.Drawing.Point(395, 240);
+            this.imagebtn.Name = "imagebtn";
+            this.imagebtn.Size = new System.Drawing.Size(101, 23);
+            this.imagebtn.TabIndex = 19;
+            this.imagebtn.Text = "Otsi fail..";
+            this.imagebtn.UseVisualStyleBackColor = true;
+            this.imagebtn.Click += new System.EventHandler(this.imagebtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(795, 412);
+            this.ClientSize = new System.Drawing.Size(1153, 633);
+            this.Controls.Add(this.imagebtn);
+            this.Controls.Add(this.PictureBox);
             this.Controls.Add(this.KustutaKat);
-            this.Controls.Add(this.Kustuta);
+            this.Controls.Add(this.kustutabtn);
             this.Controls.Add(this.refresh);
             this.Controls.Add(this.addAllbtn);
             this.Controls.Add(this.Lisabtn);
@@ -200,6 +225,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,8 +245,10 @@
         private System.Windows.Forms.Button Lisabtn;
         private System.Windows.Forms.Button addAllbtn;
         private System.Windows.Forms.Button refresh;
-        private System.Windows.Forms.Button Kustuta;
+        private System.Windows.Forms.Button kustutabtn;
         private System.Windows.Forms.Button KustutaKat;
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Button imagebtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -97,9 +98,40 @@ namespace ProdSql
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if (dgv.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgv.SelectedRows[0];
+                int selectedRowID = Convert.ToInt32(selectedRow.Cells["Id"].Value);
+
+                try
+                {
+                    if (connect.State == ConnectionState.Closed)
+                    {
+                        connect.Open();
+                    }
+                    string deleteQuery = "DELETE FROM Products WHERE Id = @id";
+                    command = new SqlCommand(deleteQuery, connect);
+                    command.Parameters.AddWithValue("@id", selectedRowID);
+                    command.ExecuteNonQuery();
+                    connect.Close();
+
+                    dgv.Rows.Remove(selectedRow);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Probleem tekkis kustutamisel: " + ex.Message);
+                }
+                finally
+                {
+                    if (connect.State == ConnectionState.Open)
+                    {
+                        connect.Close();
+                    }
+                }
+            }
 
         }
-
         private void KustutaKat_Click(object sender, EventArgs e)
         {
             if (KategooriaBox.SelectedItem !=null)
@@ -135,7 +167,8 @@ namespace ProdSql
                     command.Parameters.AddWithValue("@ProdName", ToodeBox.Text);
                     command.Parameters.AddWithValue("@Kolvo", KogusBox.Text);
                     command.Parameters.AddWithValue("@Pprice", HindBox.Text);
-                    command.Parameters.AddWithValue("@Pimage", ToodeBox.Text+".jpg");
+                    string file_pilt = ToodeBox.Text + extension;
+                    command.Parameters.AddWithValue("@Pimage", file_pilt);
                     command.Parameters.AddWithValue("@Kat", Id);
                     command.ExecuteNonQuery();
 
@@ -151,35 +184,115 @@ namespace ProdSql
             }
         }
 
+        string Kat;
+        SaveFileDialog save;
+        OpenFileDialog open;
+        string extension = null;
+
+        private void imagebtn_Click(object sender, EventArgs e)
+        {
+            open = new OpenFileDialog();
+            open.InitialDirectory = @"C:\\Users\\opilane\\Pictures";
+            open.Multiselect = true;
+            open.Filter = "Images Files(*.jpeg, *.bmp, *.png, *.jpg)|*.jpeg; *.bmp; *.png; *.jpg";
+            open.ShowDialog();
+
+            //FileInfo open_info = new FileInfo(@"C:\Users\opilane\Pictures\"+open.FileName);
+            if (open.ShowDialog() == DialogResult.OK && ToodeBox.Text != null)
+            {
+                save = new SaveFileDialog();
+                save.InitialDirectory = Path.GetFullPath(@"..\..\Images");
+                extension = Path.GetExtension(open.FileName);
+                save.FileName = ToodeBox.Text + Path.GetExtension(open.FileName);//extension
+                save.Filter = "Images" + Path.GetExtension(open.FileName) + "|" + Path.GetExtension(open.FileName);
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    File.Copy(open.FileName, save.FileName);
+                    PictureBox.Image = Image.FromFile(save.FileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Viga");
+            }
+
+        }
+
+
+
+        private void dgv_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ToodeBox.Text = dgv.Rows[e.RowIndex].Cells["ProductName"].Value.ToString();
+            KogusBox.Text = dgv.Rows[e.RowIndex].Cells["Quanty"].Value.ToString();
+            HindBox.Text = dgv.Rows[e.RowIndex].Cells["Price"].Value.ToString();
+            KategooriaBox.Text = dgv.Rows[e.RowIndex].Cells["Image"].Value.ToString();
+            if (dgv.SelectedRows.Count > 0)
+            {
+                string ImageName = dgv.SelectedRows[0].Cells["Image"].Value.ToString();
+                string ImagePath = Path.Combine(Path.GetFullPath(@"..\..\..\..\..\Pictures"), ImageName);
+                if (File.Exists(ImagePath))
+                {
+                    Image img = Image.FromFile(ImagePath);
+
+                    PictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    PictureBox.ClientSize = new Size(150, 150);
+                    PictureBox.Image = (Image)(new Bitmap(img, PictureBox.ClientSize));
+                }
+                else
+                {
+                    MessageBox.Show($"Pilt '{ImageName}' ei ole leitud",$"Путь до картинки '{ImagePath}'");
+                }
+            }
+            else
+            {
+                PictureBox.Image = null;
+            }
+        }
+
         public void Andmed()
         {
             connect.Open();
 
             DataTable dt_products = new DataTable();
-            DataTable table = new DataTable();
             adapter_products = new SqlDataAdapter("SELECT p.Id, p.ProductName, p.Quanty,p.Price, p.Image, k.Kategooria_name FROM Products as p INNER JOIN KategooriaTable as k on p.Kategooriad=k.Id;", connect);
             adapter_products.Fill(dt_products);
+            dgv.Columns.Clear();
             dgv.DataSource = dt_products;
-            table.Columns.Add("Nimetus");
-            table.Columns.Add("Kogus");
-            table.Columns.Add("Hind");
-            table.Columns.Add("Pilt");
             DataGridViewComboBoxColumn dgvcb = new DataGridViewComboBoxColumn();
-            foreach (DataRow item in dt_products.Rows)
+            //table.Columns.Add("Nimetus");
+            //table.Columns.Add("Kogus");
+            //table.Columns.Add("Hind");
+            //table.Columns.Add("Pilt");
+            //DataGridViewComboBoxColumn dgvcb = new DataGridViewComboBoxColumn();
+            dgvcb.HeaderText= "Kategooria";
+            dgvcb.Name = "KategooriaColumn";
+            dgvcb.DataPropertyName= "Kategooria";
+            HashSet<string> uncat = new HashSet<string>();
+            foreach (DataRow dr in dt_products.Rows)
             {
-                if (!dgvcb.Items.Contains(item["Kategooria_name"]))
+                string category = dr["Kategooria_name"].ToString();
+                if (!uncat.Contains(category))
                 {
-                    dgvcb.Items.Add(item["Kategooria_name"]);
+                    uncat.Add(category);
+                    dgvcb.Items.Add(category);
                 }
             }
-            foreach (DataRow item in dt_products.Rows)
-            {
-                table.Rows.Add(item["ProductName"], item["Quanty"], item["Price"], item["Image"]);
-            }
-            dgv.DataSource = table;
             dgv.Columns.Add(dgvcb);
+            dgv.Columns["Kategooria_name"].Visible = false;
+            //foreach (DataRow item in dt_products.Rows)
+            //{
+            //    if (!dgvcb.Items.Contains(item["Kategooria_name"]))
+            //    {
+            //        dgvcb.Items.Add(item["Kategooria_name"]);
+            //    }
+            //}
+            //foreach (DataRow item in dt_products.Rows)
+            //{
+            //    table.Rows.Add(item["ProductName"], item["Quanty"], item["Price"], item["Image"]);
+            //}
+            //dgv.DataSource = table;
+            //dgv.Columns.Add(dgvcb);
             connect.Close();
-
         }
     }
 }
